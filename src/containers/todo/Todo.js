20b362501a7b77c2982e992ccf393a99f8dbe6ed
Stanislav Blokh <Dnepr_horeca@list.ru>
2017-12-11 10:27:07 +0200

    css trics

--- src/containers/todo/Todo.js
+++ src/containers/todo/Todo.js
@@ -9,11 +9,13 @@ export class Todo extends React.Component{
 			description: ''
 		}
 	}
+	
+	
 	 render() {
 		 
 	return (
 	<div>	
-	<section>
+	<section className="container">
 	<div className="row">
 		<div className="col-xs12 col-md-4 col-md-offset-4">
 			<form name="todoForm">
@@ -23,12 +25,13 @@ export class Todo extends React.Component{
 					<label htmlFor="description"> Description </label>
 					<input type="text" className="form-control" id="description" name="description" placeholder="description" value={this.state.description} onChange={(e) => this.setState({description: e.target.value})}/>
 	
-	</div>
+				</div>
 				<button type="submit" className="btn btn-default" onClick={e =>  {e.preventDefault(); this.props.addTodo({title: this.state.title, description: this.state.description})}} >Submit</button>
 			</form>
 		</div>
 	</div>
 </section>
+
 	<section className="container">
 	<form className="col-xs-12 col-md-2">
 		<h4> Filter by: </h4>
@@ -36,17 +39,10 @@ export class Todo extends React.Component{
 			<label htmlFor="titleFilter"> Title</label>
 			<input type="text" className="form-control" id="titleFilter" name="titleFilter" placeholder="Title" onChange={(e => this.props.changeFilter(e.target.value))} />
 		</div>
-		<div className="form-group">
-			<label htmlFor="dateFilter">Date</label>
-			<select className="form-control" id="dateFilter" type="text" name="dateFilter">
-    		<option value=""></option>
-			<option></option>
-		</select>
-		</div>
 	</form>
 	<div className="col-xs-12 col-md-10 todo-list_wrapper">
 		<div className="row">
-			{this.props.todo.filter(f => (f.title.includes(this.props.filterString) || f.description.includes(this.props.filterString)) ).map((e, i)=>
+			{this.props.todo.filter(f => (f.title.toLowerCase().includes(this.props.filterString.toLowerCase()) || f.description.toLowerCase().includes(this.props.filterString.toLowerCase())) ).map((e, i)=>
 			<div className="col-xs-6 col-md-4 col-lg-3 todo-item" key={i}>
 				<a className="delBtn"><span onClick={()=> this.props.deleteTodo(e.id)} className="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
 				<h3>{e.title}</h3>
@@ -61,56 +57,6 @@ export class Todo extends React.Component{
 		)
 	}
 }
-
-//const Todo = (props) => (
-//	<div>	
-//	<section>
-//	<div className="row">
-//		<div className="col-xs12 col-md-4 col-md-offset-4">
-//			<form name="todoForm">
-//				<div className="form-group">
-//					<label htmlFor="title">Title</label>
-//					<input type="text" className="form-control" id="title" name="title" placeholder="Title" required />
-//					<label htmlFor="description"> Description </label>
-//					<input type="text" className="form-control" id="description" name="description" placeholder="description"/>
-//	
-//	</div>
-//				<button type="submit" className="btn btn-default" onClick={()=>console.log('click')}>Submit</button>
-//			</form>
-//		</div>
-//	</div>
-//</section>
-//	<section className="container">
-//	<form className="col-xs-12 col-md-2">
-//		<h4> Filter by: </h4>
-//		<div className="form-group">
-//			<label htmlFor="titleFilter"> Title</label>
-//			<input type="text" className="form-control" id="titleFilter" name="titleFilter" placeholder="Title" onChange={(e => props.changeFilter(e.target.value))} />
-//		</div>
-//		<div className="form-group">
-//			<label htmlFor="dateFilter">Date</label>
-//			<select className="form-control" id="dateFilter" type="text" name="dateFilter">
-//    		<option value=""></option>
-//			<option></option>
-//		</select>
-//		</div>
-//	</form>
-//	<div className="col-xs-12 col-md-10 todo-list_wrapper">
-//		<div className="row">
-//			{props.todo.filter(f => (f.title.includes(props.filterString) || f.description.includes(props.filterString)) ).map((e, i)=>
-//			<div className="col-xs-6 col-md-4 col-lg-3 todo-item" key={i}>
-//				<a className="delBtn"><span onClick={()=> props.deleteTodo(e.id)} className="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
-//				<h3>{e.title}</h3>
-//				<div>{e.description}</div>
-//				<span>{e.created_at}</span>
-//			</div> )}
-//		</div>
-//	</div>
-//</section>
-//</div>)
-//
-//export default Todo
-
 
 
 // Todo.propTypes = propTypes
